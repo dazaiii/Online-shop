b8e606831636e16ad8c8d48e675db2eb523d0593
dazaiii <kin.baryczka@gmail.com>
2021-03-25 18:39:12 +0100

    Initial commit

---
+++ src/main/java/com/kinga/onlineshop/api/UserApi.java
@@ -0,0 +1,13 @@
+package com.kinga.onlineshop.api;
+
+import com.kinga.onlineshop.dao.entity.User;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+@RestController
+public class UserApi {
+    private List users;
+
+}
