b9d0e6e435877248aff3e10e31f0435db51b70d5
dazaiii <kin.baryczka@gmail.com>
2021-04-02 18:21:34 +0200

    Creating and decoding JWT token

---
+++ src/main/java/com/kinga/onlineshop/middleware/JWT.java
@@ -0,0 +1,48 @@
+package com.kinga.onlineshop.middleware;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+public class JWT {
+    public final static String SECRET_KEY = "jhiahuw8902ujhauiakjkop1okksjoapooqija";
+
+    public static String createJWT(String id, String subject, String role, long ttlMillis){
+        //Algorytm do szyfrowania JWT
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .claim("role", role)
+                .signWith(signatureAlgorithm, signingKey);
+
+        if(ttlMillis > 0){
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        return builder.compact();
+    }
+
+    public static Claims decodeJWT(String jwt){
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+
+}
